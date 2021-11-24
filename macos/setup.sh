dd9e7c9f49ce871e13e59e4d0891bdf0849936d3
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-23 21:43:59 -0300

    Add command to quit system preferences

--- macos/setup.sh
+++ macos/setup.sh
@@ -2,7 +2,15 @@
 #
 # Mac OS system setup
 
+echo "Closing system preferences to prevent overring changes"
+osascript -e 'tell application "System Preferences" to quit'
+echo "System preferences closed."
+
 # Enable dark mode, even it it's already enabled
 echo "Enabling dark mode.."
 osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
-echo "Dark mode enabled"
+echo "Dark mode enabled."
+
+echo "Setting dock position to right.."
+defaults write com.apple.dock orientation -string "right"
+echo "Dock position set to right."
