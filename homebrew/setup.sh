684a12cc1627b435e04268cda930e009d5251a95
Breno Battaglin <breno.battaglin@icloud.com>
2021-11-26 22:40:06 -0300

    Create homebrew setup

---
+++ homebrew/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Mac OS system setup
+
+#  Formulae
+brew install curl
+brew install git
+brew install git-lfs
+
+# Casks
+brew install --cask authy
+brew install --cask discord
+brew install --cask docker
+brew install --cask figma
+brew install --cask firefox
+brew install --cask gimp
+brew install --cask microsoft-office
+brew install --cask postman
+brew install --cask spotify
+brew install --cask visual-studio-code
+brew install --cask vlc
