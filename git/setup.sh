c944754341f4a86c2f1ae560487b90fb8db7defc
Breno Battaglin <breno.battaglin@icloud.com>
2021-12-06 18:14:21 -0300

    Update git setup

--- git/setup.sh
+++ git/setup.sh
@@ -1,4 +1,9 @@
+git config --global user.name "your name and last name"
+git config --global user.email "your email here"
+
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+
+ssh-keygen -t ed25519 -C "your email here"
