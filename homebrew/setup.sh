2c4e54ddb2bdfd6ccbd647020819dc26e6f37b9b
Breno Battaglin <breno.battaglin@icloud.com>
2021-12-06 20:18:46 -0300

    Update homebrew casks
    - Replace ms office by libreoffice
    - Add flutter
    - Add malwarebytes

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -13,8 +13,10 @@ brew install --cask discord
 brew install --cask docker
 brew install --cask figma
 brew install --cask firefox
+brew install --cask flutter
 brew install --cask gimp
-brew install --cask microsoft-office
+brew install --cask libreoffice
+brew install --cask malwarebytes
 brew install --cask postman
 brew install --cask spotify
 brew install --cask visual-studio-code
