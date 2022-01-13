df109c07e4ef7e1ab68e363721aac5798ec23126
Breno Battaglin <breno.battaglin@icloud.com>
2022-01-12 21:08:00 -0300

    Create oh-my-zsh setup script and dotfile

---
+++ oh-my-zsh/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install zsh-autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install zsh-syntax-highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Copy .zshrc file to the home dir
+cp .zshrc ~/.zshrc
