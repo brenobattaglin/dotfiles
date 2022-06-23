69d4fa53ed7757abd1938b7f3871fc4b167009b1
Breno Battaglin <breno.battaglin@icloud.com>
2022-06-23 18:01:37 -0300

    Update homebrew setup

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -4,6 +4,7 @@
 #  Formulaes
 brew install cocoapods
 brew install curl
+brew install docker-compose
 brew install exiftool
 brew install git
 brew install git-lfs
@@ -11,6 +12,8 @@ brew install neofetch
 
 # Casks
 brew install --cask android-studio
+brew install --cask authy
+brew install --cask balenaetcher
 brew install --cask brave-browser
 brew install --cask caffeine
 brew install --cask discord
@@ -19,10 +22,11 @@ brew install --cask dozer
 brew install --cask flutter
 brew install --cask gimp
 brew install --cask malwarebytes
+brew install --cask obsidian
 brew install --cask postman
 brew install --cask rectangle
 brew install --cask spotify
-brew install --cask steam
 brew install --cask transmission
+brew install --cask the-unarchiver
 brew install --cask veracrypt
 brew install --cask visual-studio-code
