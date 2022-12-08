005f40fabf4f3eccef3112270cd2b633845df07c
Breno Battaglin <breno.battaglin@icloud.com>
2022-12-07 22:01:17 -0300

    Update casks
    Add the following casks:
    - beekeper
    - iina
    - iterm2
    - kindle
    - macfuse
    - visual-studio

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -6,6 +6,7 @@ brew install --cask android-studio
 brew install --cask authy
 brew install --cask balenaetcher
 brew install --cask brave-browser
+brew install --cask beekeeper-studio
 brew install --cask caffeine
 brew install --cask discord
 brew install --cask docker
@@ -13,7 +14,11 @@ brew install --cask dozer
 brew install --cask exodus
 brew install --cask flutter
 brew install --cask gimp
+brew install --cask iina
+brew install --cask iterm2
+brew install --cask kindle
 brew install --cask maccy
+brew install --cask macfuse
 brew install --cask obsidian
 brew install --cask postman
 brew install --cask rectangle
@@ -21,6 +26,7 @@ brew install --cask spotify
 brew install --cask transmission
 brew install --cask the-unarchiver
 brew install --cask veracrypt
+brew install --cask visual-studio
 brew install --cask visual-studio-code
 
 #  Formulaes
