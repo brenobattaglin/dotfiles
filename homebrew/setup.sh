036262f18883efe185170b0faf132eb5f490c4ef
Breno Battaglin <breno.battaglin@icloud.com>
2023-01-05 10:57:22 -0300

    Refactor homebrew setup
    - Create for loops

--- homebrew/setup.sh
+++ homebrew/setup.sh
@@ -1,40 +1,55 @@
 #!/bin/sh
 
+casks = (
+    "android-studio"
+    "authy"
+    "balenaetcher"
+    "brave-browser"
+    "beekeeper-studio"
+    "caffeine"
+    "discord"
+    "docker"
+    "dozer"
+    "exodus"
+    "flutter"
+    "gimp"
+    "iina"
+    "iterm2"
+    "kindle"
+    "maccy"
+    "macfuse"
+    "obsidian"
+    "postman"
+    "rectangle"
+    "spotify"
+    "transmission"
+    "the-unarchiver"
+    "veracrypt"
+    "visual-studio"
+    "visual-studio-code"
+)
 
-# Casks
-brew install --cask android-studio
-brew install --cask authy
-brew install --cask balenaetcher
-brew install --cask brave-browser
-brew install --cask beekeeper-studio
-brew install --cask caffeine
-brew install --cask discord
-brew install --cask docker
-brew install --cask dozer
-brew install --cask exodus
-brew install --cask flutter
-brew install --cask gimp
-brew install --cask iina
-brew install --cask iterm2
-brew install --cask kindle
-brew install --cask maccy
-brew install --cask macfuse
-brew install --cask obsidian
-brew install --cask postman
-brew install --cask rectangle
-brew install --cask spotify
-brew install --cask transmission
-brew install --cask the-unarchiver
-brew install --cask veracrypt
-brew install --cask visual-studio
-brew install --cask visual-studio-code
+formulaes = (
+    "cocoapods"
+    "curl"
+    "docker-compose"
+    "dotnet"
+    "exiftool"
+    "git"
+    "git-lfs"
+    "neofetch"
+)
 
-#  Formulaes
-brew install cocoapods
-brew install curl
-brew install docker-compose
-brew install dotnet
-brew install exiftool
-brew install git
-brew install git-lfs
-brew install neofetch
+for c in ${casks[@]};
+do
+    (
+        brew install --cask "$c"
+    )
+done
+
+for f in ${formulaes[@]};
+do
+    (
+        brew install "$f"
+    )
+done
